f3dd53da346e48e35ca7b385bed6596d68cc2ef6
Justin Lin <justinwlin@gmail.com>
2023-12-20 15:46:05 -0800

    Update runpod_client_helper.py

--- runpod_client_helper.py
+++ runpod_client_helper.py
@@ -1,6 +1,10 @@
 import time
 import requests
 import json
+import ffmpeg
+import os
+import base64
+import tempfile
 
 
 class NoOutputFromRunpodException(Exception):
@@ -98,3 +102,100 @@ def transcribe_audio(base64_string, runpod_api_key, server_endpoint, polling_int
     job_id = send_async_transcription_request(base64_string, runpod_api_key, server_endpoint)
     return wait_for_transcription_completion(job_id, runpod_api_key, server_endpoint, polling_interval)
 
+
+def convert_to_mp3_and_base64(input_path):
+    """
+    Converts an audio or video file to MP3 format with specified settings, 
+    then encodes the MP3 file to a Base64 string.
+
+    This function takes an input file path, converts the file to a mono MP3 
+    file with a bitrate of 32k, and encodes this MP3 file to a Base64 string.
+    It handles the creation and deletion of temporary files used during the conversion process.
+
+    Args:
+    input_path (str): The file path of the input audio or video file.
+
+    Returns:
+    str: A Base64 encoded string of the converted MP3 file.
+
+    Raises:
+    ffmpeg.Error: If an error occurs during the conversion process.
+    """
+    try:
+        # Create a temporary file for the MP3
+        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as temp_mp3_file:
+            output_mp3_path = temp_mp3_file.name
+
+            # Convert the file to MP3
+            (
+                ffmpeg
+                .input(input_path)
+                .output(output_mp3_path, ac=1, ar='22050', ab='32k', format='mp3')
+                .run(overwrite_output=True)
+            )
+            print(f"Conversion to MP3 successful. Temp file created at {output_mp3_path}")
+
+            # Encode the MP3 file directly in Base64
+            with open(output_mp3_path, "rb") as mp3_file:
+                base64_encoded_data = base64.b64encode(mp3_file.read()).decode('utf-8')
+
+        # Get the size of the Base64 string
+        base64_size = len(base64_encoded_data.encode('utf-8')) / (1024 * 1024)  # size in MB
+        print(f"Base64 Encoded Size: {base64_size:.2f} MB")
+
+        # Clean up the temporary MP3 file
+        os.remove(output_mp3_path)
+
+        return [base64_encoded_data, base64_size]
+
+    except ffmpeg.Error as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+def decode_base64_to_mp3(base64_data, output_mp3_path):
+    """
+    Decodes a Base64 encoded string back into an MP3 file.
+
+    This function takes a Base64 encoded string, representing an MP3 file,
+    decodes it, and writes the result to a specified output file path. It is
+    intended to be used as a reversal of the convert_to_mp3_and_base64 function.
+
+    Args:
+    base64_data (str): The Base64 encoded string of the MP3 file.
+    output_mp3_path (str): The file path where the decoded MP3 file will be saved.
+
+    Returns:
+    None: The function does not return a value but saves the decoded MP3 file at the specified path.
+
+    Raises:
+    Exception: If any error occurs during the decoding and file writing process.
+    """
+    try:
+        # Decode the Base64 string
+        mp3_data = base64.b64decode(base64_data)
+
+        # Write the decoded data to an MP3 file
+        with open(output_mp3_path, 'wb') as mp3_file:
+            mp3_file.write(mp3_data)
+
+        print(f"MP3 file created at {output_mp3_path}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def checkFileSize(input_path):
+    """
+    Checks the size of an input file and returns the size in MB.
+    """
+    
+    try:
+        # Get the size of the input file
+        input_size = os.path.getsize(input_path) / (1024 * 1024)  # size in MB
+        print(f"Input File Size: {input_size:.2f} MB")
+
+        # Return in MB to 2 decimal places
+        return round(input_size, 2)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
