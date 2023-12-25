bed1abce53410d2c58430e7d32fb796f9784eb5e
Justin Lin <justinwlin@gmail.com>
2023-12-25 14:31:12 -0800

    runpod serverless update functions

--- asyncio_runpod_client_helper.py
+++ asyncio_runpod_client_helper.py
@@ -2,6 +2,7 @@ import asyncio
 import aiohttp
 import json
 
+
 class RunpodApiClient:
     def __init__(self, api_key, server_endpoint):
         self.api_key = api_key
@@ -10,20 +11,27 @@ class RunpodApiClient:
     class RunpodApiException(Exception):
         """Exception raised when there is an issue with the Runpod API request."""
 
-    async def send_async_api_request(self, input_payload, session):
-        """ Sends an asynchronous request to the specified Runpod API endpoint. """
+    async def send_async_api_request(
+        self, input_payload, session, execution_timeout=600000
+    ):  # 10 minutes in milliseconds
+        """Sends an asynchronous request to the specified Runpod API endpoint."""
         url = f"https://api.runpod.ai/v2/{self.server_endpoint}/run"
-        payload = json.dumps({"input": input_payload})
+        payload = {
+            "input": input_payload,
+            "policy": {"executionTimeout": execution_timeout},
+        }
+        json_payload = json.dumps(payload)
+
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}",
         }
-        async with session.post(url, headers=headers, data=payload) as response:
+        async with session.post(url, headers=headers, data=json_payload) as response:
             response_json = await response.json()
             return response_json["id"]
 
     async def get_api_request_status(self, job_id, session):
-        """ Gets the status of an API request job. """
+        """Gets the status of an API request job."""
         url = f"https://api.runpod.ai/v2/{self.server_endpoint}/status/{job_id}"
         headers = {
             "Content-Type": "application/json",
@@ -32,8 +40,10 @@ class RunpodApiClient:
         async with session.get(url, headers=headers) as response:
             return await response.json()
 
-    async def wait_for_api_request_completion(self, job_id, session, polling_interval=20):
-        """ Waits for the API request job to complete and returns the output. """
+    async def wait_for_api_request_completion(
+        self, job_id, session, polling_interval=20
+    ):
+        """Waits for the API request job to complete and returns the output."""
         while True:
             status_response = await self.get_api_request_status(job_id, session)
             status = status_response["status"]
@@ -51,15 +61,26 @@ class RunpodApiClient:
                         f"API request job failed with status: {status}"
                     )
 
-    async def execute_async_api_request(self, input_payload, session, polling_interval=20):
-        """ Helper method for executing an asynchronous API request. """
-        job_id = await self.send_async_api_request(input_payload, session)
-        return await self.wait_for_api_request_completion(job_id, session, polling_interval)
+    async def execute_async_api_request(
+        self, input_payload, session, polling_interval=20, execution_timeout=600000
+    ):
+        """Helper method for executing an asynchronous API request."""
+        job_id = await self.send_async_api_request(
+            input_payload, session, execution_timeout
+        )
+        return await self.wait_for_api_request_completion(
+            job_id, session, polling_interval
+        )
 
-    def execute_sync_api_request(self, input_payload, polling_interval=20):
-        """ Synchronously executes an API request using the provided service API. """
+    def execute_sync_api_request(
+        self, input_payload, polling_interval=20, execution_timeout=600000
+    ):
+        """Synchronously executes an API request using the provided service API."""
+
         async def sync_wrapper():
             async with aiohttp.ClientSession() as session:
-                return await self.execute_async_api_request(input_payload, session, polling_interval)
+                return await self.execute_async_api_request(
+                    input_payload, session, polling_interval, execution_timeout
+                )
 
         return asyncio.run(sync_wrapper())
--- runpod_client_helper.py
+++ runpod_client_helper.py
@@ -11,7 +11,50 @@ class NoOutputFromRunpodException(Exception):
     """Exception raised when there is no output from Runpod."""
 
 
-def send_async_transcription_request(base64_string_or_url, api_key, server_endpoint):
+def check_health(api_key, server_endpoint):
+    """
+    Checks health and worker statistics of a particular endpoint.
+
+    Args:
+        api_key (str): Runpod API key.
+        server_endpoint (str): Server endpoint.
+
+    Returns:
+        dict: Health statistics response from Runpod.
+    """
+    url = f"https://api.runpod.ai/v2/{server_endpoint}/health"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    response = requests.get(url, headers=headers).json()
+    return response
+
+
+def cancel_job(job_id, api_key, server_endpoint):
+    """
+    Cancels a transcription job given its job ID.
+
+    Args:
+        job_id (str): Job ID of the transcription request to cancel.
+        api_key (str): Runpod API key.
+        server_endpoint (str): Server endpoint.
+
+    Returns:
+        dict: Cancellation response from Runpod.
+    """
+    url = f"https://api.runpod.ai/v2/{server_endpoint}/cancel/{job_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    response = requests.post(url, headers=headers).json()
+    return response
+
+
+def send_async_transcription_request(
+    base64_string_or_url, api_key, server_endpoint, execution_timeout=600000
+):
     """
     Sends an asynchronous transcription request to Runpod.
 
@@ -19,6 +62,7 @@ def send_async_transcription_request(base64_string_or_url, api_key, server_endpo
         base64_string_or_url (str): Base64-encoded audio data or a URL that starts with "http".
         api_key (str): Runpod API key.
         server_endpoint (str): Server endpoint.
+        execution_timeout (int): Execution timeout in milliseconds, default is 600,000 (10 minutes).
 
     Returns:
         str: Job ID of the transcription request.
@@ -28,10 +72,13 @@ def send_async_transcription_request(base64_string_or_url, api_key, server_endpo
     payload_base64 = {"audio_base_64": base64_string_or_url}
     payload_url = {"audio_url": base64_string_or_url}
 
-    if(base64_string_or_url.startswith("http")):
-        payload = json.dumps({"input": payload_url})
+    policy = {"executionTimeout": execution_timeout}
+
+    if base64_string_or_url.startswith("http"):
+        payload = json.dumps({"input": payload_url, "policy": policy})
     else:
-        payload = json.dumps({"input": payload_base64})
+        payload = json.dumps({"input": payload_base64, "policy": policy})
+
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}",
@@ -94,7 +141,9 @@ def wait_for_transcription_completion(
                 )
 
 
-def transcribe_audio(base64_string_or_url, runpod_api_key, server_endpoint, polling_interval=20):
+def transcribe_audio(
+    base64_string_or_url, runpod_api_key, server_endpoint, polling_interval=20
+):
     """
     Transcribes audio using Runpod's API.
 
@@ -106,16 +155,20 @@ def transcribe_audio(base64_string_or_url, runpod_api_key, server_endpoint, poll
     Returns:
         dict: Transcription output or status.
     """
-    job_id = send_async_transcription_request(base64_string_or_url, runpod_api_key, server_endpoint)
-    return wait_for_transcription_completion(job_id, runpod_api_key, server_endpoint, polling_interval)
+    job_id = send_async_transcription_request(
+        base64_string_or_url, runpod_api_key, server_endpoint
+    )
+    return wait_for_transcription_completion(
+        job_id, runpod_api_key, server_endpoint, polling_interval
+    )
 
 
 def convert_to_mp3_and_base64(input_path):
     """
-    Converts an audio or video file to MP3 format with specified settings, 
+    Converts an audio or video file to MP3 format with specified settings,
     then encodes the MP3 file to a Base64 string.
 
-    This function takes an input file path, converts the file to a mono MP3 
+    This function takes an input file path, converts the file to a mono MP3
     file with a bitrate of 32k, and encodes this MP3 file to a Base64 string.
     It handles the creation and deletion of temporary files used during the conversion process.
 
@@ -130,24 +183,27 @@ def convert_to_mp3_and_base64(input_path):
     """
     try:
         # Create a temporary file for the MP3
-        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as temp_mp3_file:
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_mp3_file:
             output_mp3_path = temp_mp3_file.name
 
             # Convert the file to MP3
             (
-                ffmpeg
-                .input(input_path)
-                .output(output_mp3_path, ac=1, ar='22050', ab='32k', format='mp3')
+                ffmpeg.input(input_path)
+                .output(output_mp3_path, ac=1, ar="22050", ab="32k", format="mp3")
                 .run(overwrite_output=True)
             )
-            print(f"Conversion to MP3 successful. Temp file created at {output_mp3_path}")
+            print(
+                f"Conversion to MP3 successful. Temp file created at {output_mp3_path}"
+            )
 
             # Encode the MP3 file directly in Base64
             with open(output_mp3_path, "rb") as mp3_file:
-                base64_encoded_data = base64.b64encode(mp3_file.read()).decode('utf-8')
+                base64_encoded_data = base64.b64encode(mp3_file.read()).decode("utf-8")
 
         # Get the size of the Base64 string
-        base64_size = len(base64_encoded_data.encode('utf-8')) / (1024 * 1024)  # size in MB
+        base64_size = len(base64_encoded_data.encode("utf-8")) / (
+            1024 * 1024
+        )  # size in MB
         print(f"Base64 Encoded Size: {base64_size:.2f} MB")
 
         # Clean up the temporary MP3 file
@@ -158,7 +214,8 @@ def convert_to_mp3_and_base64(input_path):
     except ffmpeg.Error as e:
         print(f"An error occurred: {e}")
         return None
-    
+
+
 def decode_base64_to_mp3(base64_data, output_mp3_path):
     """
     Decodes a Base64 encoded string back into an MP3 file.
@@ -182,7 +239,7 @@ def decode_base64_to_mp3(base64_data, output_mp3_path):
         mp3_data = base64.b64decode(base64_data)
 
         # Write the decoded data to an MP3 file
-        with open(output_mp3_path, 'wb') as mp3_file:
+        with open(output_mp3_path, "wb") as mp3_file:
             mp3_file.write(mp3_data)
 
         print(f"MP3 file created at {output_mp3_path}")
@@ -190,11 +247,12 @@ def decode_base64_to_mp3(base64_data, output_mp3_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
 def checkFileSize(input_path):
     """
     Checks the size of an input file and returns the size in MB.
     """
-    
+
     try:
         # Get the size of the input file
         input_size = os.path.getsize(input_path) / (1024 * 1024)  # size in MB
